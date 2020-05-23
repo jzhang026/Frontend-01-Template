4c35f77ec10c6d10d01592a6db28a614711d5f6d
Adrian Zhang <adrian.zhang@shopee.com>
2020-05-23 20:50:47 +0800

    save

--- week06/KMP-state-machine/kmp-with-state-machine.js
+++ week06/KMP-state-machine/kmp-with-state-machine.js
@@ -1,8 +1,11 @@
 // 我们可以用当前state， 下一个输入的字符，以及模式串 来算出下一个状态
+// 我们这里状态机的状态，就是表示我们已经匹配到了模式串的第几个字符
 function getNextState(pattern, state, currentCharacter) {
   if (state < pattern.length && currentCharacter == pattern[state])
     return state + 1;
   // 如果没匹配，找最长前缀,放入状态转移table
+  // 最长前缀就是KMP中的所谓的好前缀与坏字符
+  // 好前缀的长度就是我们下一个状态要转移去的状态
   for (let prefixLength = state - 1; prefixLength > 0; prefixLength--) {
     for (let i = 0; i <= prefixLength; i++) {
       if (pattern[i + 1] !== pattern[prefixLength]) break;
