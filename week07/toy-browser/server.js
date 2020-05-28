87cd015c1b5bfe2d50be41388b8f5db0b5540d47
Adrian Zhang <adrian.zhang@shopee.com>
2020-05-28 20:12:55 +0800

    save

--- week07/toy-browser/server.js
+++ week07/toy-browser/server.js
@@ -4,73 +4,34 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`<html maaa="a">
+  res.end(`<html maaa=a >
   <head>
     <style>
-      #container {
-        width: 500px;
-        height: 300px;
-        display: flex;
-        background-color: rgb(255, 255, 255);
+      #container{
+        width:500px;
+        height:300px;
+        display:flex;
+        background-color:rgb(255,255,255);
       }
-      #container #myid {
-        width: 200px;
-        height: 100px;
-        background-color: rgb(255, 0, 0);
-        display: flex;
+      #container #myid{
+        width:200px;
+        height:100px;
+        background-color:rgb(255,0,0)
       }
-      #myid > p {
-        flex: 1;
-        align-content: stretch;
-        background-color: rgb(70, 52, 235);
-        width: 50px;
-      }
-      #myid ~ p {
-        align-content: stretch;
-        background-color: rgb(25, 152, 185);
-        width: 50px;
-        height: 50px;
-        display: flex;
-      }
-      p ~ span {
-        background-color: rgb(226, 52, 235);
-        width: 25px;
-        height: 50px;
-      }
-      #container .c1 {
-        background-color: rgb(0, 255, 0);
+      #container .c1{
+        flex:1;
+        background-color:rgb(0,255,0)
       }
     </style>
   </head>
   <body>
-    <div id="container" >
-      <div id="myid" >
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-      </div>
+    <div id="container">
+      <div id="myid"></div>
       <div class="c1"></div>
-      <p></p>
-      <span></span>
-      <p></p>
-      <span></span>
-      <p></p>
-      <span></span>
-      <p></p>
-      <span></span>
     </div>
   </body>
-</html>
-`);
+
+</html>`);
 });
 
 server.listen(8088);
